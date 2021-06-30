438b824743eefe9d3bd337ba377a966a15918ed2
brenodev92 <breno.gs.lopes@gmail.com>
2021-06-30 16:36:24 -0300

    feat: criando componente para renderizar dados do servidor

---
+++ src/screens/AppointmentDetails/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+import { BasePage } from '../../components'
+
+
+export default function AppointmentDetails() {
+  return (
+    <BasePage>
+      <Text>Ola</Text>
+    </BasePage>
+  )
+}
